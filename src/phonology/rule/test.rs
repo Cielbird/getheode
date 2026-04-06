5c83cea42e58c8b5f3cc04f1829e8c367cf63e70
Cielbird <guilhem.ane3@gmail.com>
2026-04-06 16:02:53 -0400

    added phonorule test

--- src/phonology/rule/test.rs
+++ src/phonology/rule/test.rs
@@ -1,6 +1,6 @@
 use crate::{
     phonology::{
-        feature::FeatureState::*,
+        feature::FeatureState::{self, *},
         rule::{PhonoRule, SegmentInfo, SyllableInfo},
         segment::{SEG_FEATURE_COUNT, SegmentFeatures},
         string::PhonoString,
@@ -13,8 +13,16 @@ const VOWEL_SEG: SegmentFeatures = SegmentFeatures::from_features([
     POS, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF,
     UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF,
 ]);
+const CONS_SEG: SegmentFeatures = SegmentFeatures::from_features([
+    NEG, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF,
+    UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF,
+]);
 const T_SEG: SegmentFeatures = SegmentFeatures::from_features([
     NEG, NEG, POS, NEG, NEG, NEG, NEG, NEG, NEG, NEG, NEG, NEG, NEG, NEG, NEG, NEG, POS, POS, NEG,
+    NEG, NEG, NEG, NA, NA, NA, NA, NA,
+]);
+const D_SEG: SegmentFeatures = SegmentFeatures::from_features([
+    NEG, NEG, POS, NEG, NEG, NEG, NEG, NEG, NEG, NEG, POS, NEG, NEG, NEG, NEG, NEG, POS, POS, NEG,
     NEG, NEG, NEG, NA, NA, NA, NA, NA,
 ]);
 const A_SEG: SegmentFeatures = SegmentFeatures::from_features([
@@ -24,6 +32,10 @@ const A_SEG: SegmentFeatures = SegmentFeatures::from_features([
 const I_SEG: SegmentFeatures = SegmentFeatures::from_features([
     POS, NEG, NEG, POS, POS, NEG, POS, NEG, NEG, NEG, POS, NEG, NEG, NEG, NEG, NEG, NEG, NA, NA,
     NA, NEG, POS, POS, NEG, POS, NEG, POS,
+]);
+const VOI_SEG: SegmentFeatures = SegmentFeatures::from_features([
+    UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, POS, UNDEF, UNDEF, UNDEF,
+    UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF, UNDEF,
 ]);
 const UNDEF_SEG: SegmentFeatures =
     SegmentFeatures::from_features([UNDEF; SEG_FEATURE_COUNT as usize]);
@@ -72,6 +84,54 @@ fn test_rule_simple() {
     let expected_replace_with = PhonoString::new(ud3tree![
         () => [
             UNSTRESSED => [A_SEG, I_SEG],
+        ]
+    ]);
+    assert_eq!(pat_match.replace_with, expected_replace_with)
+}
+
+#[test]
+fn test_rule_modify_vowel() {
+    // rule follows this rule:
+    // VCV => VC[+voi]V / (all in same syllable and word)
+    // for example, [ati] becomes [adi]
+    let match_tree = ud3tree![
+        () => [
+            SyllableInfo {id: Some(0), features: UNDEF_SYL} => [
+                SegmentInfo {id: Some(1), features: VOWEL_SEG},
+                SegmentInfo {id: Some(2), features: CONS_SEG},
+                SegmentInfo {id: Some(3), features: VOWEL_SEG},
+            ]
+        ]
+    ];
+    let replace_tree = ud3tree![
+        () => [
+            SyllableInfo {id: Some(0), features: UNDEF_SYL} => [
+                SegmentInfo {id: Some(1), features: VOWEL_SEG},
+                SegmentInfo {id: Some(2), features: VOI_SEG},
+                SegmentInfo {id: Some(3), features: VOWEL_SEG},
+            ]
+        ]
+    ];
+    let rule = PhonoRule::new(match_tree, replace_tree);
+
+    // [ta.ati]
+    let hay = PhonoString::new(ud3tree![
+        () => [
+            UNSTRESSED => [T_SEG, A_SEG],
+            UNSTRESSED => [A_SEG, T_SEG, I_SEG],
+        ]
+    ]);
+
+    let mut matches = rule.find(hay);
+
+    assert_eq!(matches.len(), 1);
+    let pat_match = matches.remove(0);
+
+    assert_eq!(pat_match.range, 2..5);
+
+    let expected_replace_with = PhonoString::new(ud3tree![
+        () => [
+            UNSTRESSED => [A_SEG, D_SEG, I_SEG],
         ]
     ]);
     assert_eq!(pat_match.replace_with, expected_replace_with)
